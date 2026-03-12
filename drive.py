d9f9fd4c3b9e0bc6fc9c094b9fe9856068512820
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-12 16:48:31 +0000

    added algebraic distance

--- drive.py
+++ drive.py
@@ -14,10 +14,14 @@ R_WHEEL = 2.2       # wheel radius in cm
 MIN_SPEED = 90      # wheel rotation speed in degrees.s-1
 
 def move(left, right, distance, speed=MIN_SPEED):
-    n_rotations = distance/(R_WHEEL * math.pi * 2)
+    n_rotations = abs(distance/(R_WHEEL * math.pi * 2))
     spin_time = (n_rotations*360)/speed
-    left.set_dps(speed)
-    right.set_dps(speed)
+    if distance < 0:
+        direction = 1
+    else:
+        direction = -1
+    left.set_dps(direction * speed)
+    right.set_dps(direction * speed)
     time.sleep(spin_time)
 
 
