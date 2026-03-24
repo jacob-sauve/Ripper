15f259a300a338e627e7db2abded5e8f99b57767
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 17:55:04 -0400

    trying compound movement

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -133,6 +133,7 @@ class Megamind(Processor):
         while True: 
             instruction, *args = self.queue.get()
             #instruction, *args = self.queue.get()
+            print(instruction)
             if instruction:
                 # call function
                 self.funcdict.get(instruction)(*args)
@@ -243,7 +244,7 @@ class Driver(Processor):
         # set default speed value
         try:
             power = 100*(self.direction * speed)/self.motor_pin.MAX_SPEED if speed is not None else 100*self.min_speed/self.motor_pin.MAX_SPEED
-            print(power)
+            #print(power)
             self.motor_pin.set_power(power)
             self.is_moving = True
            #print(self.name + " moving")
@@ -350,7 +351,7 @@ if __name__ == "__main__":
         import titlecard
         titlecard.show()
         print(f"{cpu_count()=}\n\n")
-        brain.queue.put(("GO", 10))
+        brain.queue.put(("GO", 20))
         brain.queue.put(("GRAB", 5, 500))
         while not stop.is_pressed():
             sleep(0.01)
