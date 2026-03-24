86ec576e65123b604877c8392c721893f5cf4695
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 19:10:37 -0400

    test of grab + sweep

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -391,8 +391,9 @@ if __name__ == "__main__":
         import titlecard
         titlecard.show()
         print(f"{cpu_count()=}\n\n")
-        brain.queue.put_nowait(("GO", 20, 270))
+        #brain.queue.put_nowait(("GO", 20, 270))
         brain.queue.put_nowait(("GRAB", 10, 500))
+        brain.queue.put_nowait(("SWEEP", 180, True))
         while not stop.is_pressed():
             sleep(0.01)
         raise Exception()
