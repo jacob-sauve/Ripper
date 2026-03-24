1f8f5723a7a6a8036ee1f107d6635b92dd03e806
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 17:30:04 -0400

    trying hardcoding motor power

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -243,7 +243,7 @@ class Driver(Processor):
         # set default speed value
         try:
             power = (self.direction * speed)/self.motor_pin.MAX_SPEED if speed is not None else self.min_speed/self.motor_pin.MAX_SPEED
-            self.motor_pin.set_power(power)
+            self.motor_pin.set_power(30)
             self.is_moving = True
            #print(self.name + " moving")
             return True
