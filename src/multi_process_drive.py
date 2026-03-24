31dae8e7c0792edb226645b7f862b985a81161db
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 18:25:27 -0400

    fix empty check

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -124,7 +124,7 @@ class Megamind(Processor):
                 #queue_front = sensor.queue.get()
                 except:
                     queue_front = None
-                while not (sensor.queue.Empty):
+                while not (sensor.queue.empty()):
                     queue_front_dict[sensor] = queue_front
                     queue_front = sensor.queue.get_nowait()
         return queue_front_dict
