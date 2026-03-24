4216f35a5ece7565dc5e8b03e3afcaa73818c651
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 19:40:24 -0400

    test with 1/2 range of motion

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -402,7 +402,7 @@ if __name__ == "__main__":
         print(f"{cpu_count()=}\n\n")
         #brain.queue.put_nowait(("GO", 20, 270))
         brain.queue.put_nowait(("GRAB", 10, 500))
-        brain.queue.put_nowait(("SWEEP", 180, True, 100))
+        brain.queue.put_nowait(("SWEEP", 90, True, 100)
         while not stop.is_pressed():
             sleep(0.01)
         raise Exception()
