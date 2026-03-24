70b625ac4189c5c9e3ccdf6c4a5c336fdeaefc2a
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 19:55:57 -0400

    remove wait_done calls

--- src/musical.py
+++ src/musical.py
@@ -16,14 +16,14 @@ def victor_jingle():
     tone1 = Sound(duration = rhythm/8, pitch = "A3", volume=100)
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("B3")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("C4")
@@ -31,14 +31,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("A3")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("F3")
@@ -46,7 +46,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("G3")
@@ -54,7 +54,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
 
@@ -63,14 +63,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("D4")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("Eb4")
@@ -78,14 +78,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("C4")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("G#3")
@@ -93,7 +93,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("Bb3")
@@ -101,7 +101,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
 
@@ -110,14 +110,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("F4")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("F#4")
@@ -125,14 +125,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("Eb4")
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("B3")
@@ -140,7 +140,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("Db4")
@@ -148,14 +148,14 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
     tone1.set_pitch("F#4")
     tone1.update_duration(rhythm*3/8)
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 
 
     tone1.set_pitch("G#4")
@@ -163,7 +163,7 @@ def victor_jingle():
     tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+    #tone1.wait_done()
 #!/usr/bin/env python3
 
 
