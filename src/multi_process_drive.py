b04f1743a96e7d3f2a0d8d430910d2f19a532fd4
jacob-sauve <woozy-army-trivial@duck.com>
2026-03-24 18:07:06 -0400

    attempt with negative speed

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -350,7 +350,7 @@ if __name__ == "__main__":
         import titlecard
         titlecard.show()
         print(f"{cpu_count()=}\n\n")
-        brain.queue.put(("GO", 20, 270))
+        brain.queue.put(("GO", 20, -270))
         brain.queue.put(("GRAB", 5, 500))
         while not stop.is_pressed():
             sleep(0.01)
