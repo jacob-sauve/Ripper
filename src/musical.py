d14f8d7af889e6aa4480c3a2b7bdee5c65fcc1e0
IvanGaspart <ivan.torrefacteuramical@gmail.com>
2026-03-24 20:06:03 -0400

    updated victory jingle

--- src/musical.py
+++ src/musical.py
@@ -16,154 +16,85 @@ def victor_jingle():
     tone1 = Sound(duration = rhythm/8, pitch = "A3", volume=100)
 
     tone1.play()
-    tone1.wait_done()
-
 
     tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "B3")
-    tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "C4")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "A3")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm/4, volume = 100, pitch = "F3")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm/2, volume = 100, pitch = "G3")
+
+    tone1.play()
 
 
     tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "C4")
-    tone1.update_duration(rhythm*3/8) 
-    tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "A3")
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "F3")
-    tone1.update_duration(rhythm/4)
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "G3")
-    tone1.update_duration(rhythm/2)
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "C4")
-    tone1.update_duration(rhythm/8)
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
 
     tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "D4")
-    tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
+
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "Eb4")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "C4")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm/4, volume = 100, pitch = "G#3")
+
+    tone1.play()
+
+    tone1 = Sound(duration = rhythm/2, volume = 100, pitch = "Bb3")
+
+    tone1.play()
 
 
     tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "Eb4")
-    tone1.update_duration(rhythm*3/8)
-    tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "C4")
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "G#3")
-    tone1.update_duration(rhythm/4)
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "Bb3")
-    tone1.update_duration(rhythm/2)
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
-
-
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "Eb4")
-    tone1.update_audio()
-
-    tone1.play()
-    tone1.wait_done()
-
 
     tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "F4")
-    tone1.update_audio()
 
     tone1.play()
-    tone1.wait_done()
 
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "F#4")
-    tone1.update_duration(rhythm*3/8)
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "F#4")
 
     tone1.play()
-    tone1.wait_done()
 
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "Eb4")
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "Eb4")
 
     tone1.play()
-    tone1.wait_done()
 
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "B3")
-    tone1.update_duration(rhythm/4)
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm/4, volume = 100, pitch = "B3")
 
     tone1.play()
-    tone1.wait_done()
 
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "Db4")
-    tone1.update_duration(rhythm*3/8)
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "Db4")
 
     tone1.play()
-    tone1.wait_done()
 
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "F#4")
-    tone1.update_duration(rhythm*3/8)
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm*3/8, volume = 100, pitch = "F#4")
 
     tone1.play()
-    tone1.wait_done()
 
-
-    tone1 = Sound(duration = rhythm/8, volume = 100, pitch = "G#4")
-    tone1.update_duration(rhythm)
-    tone1.update_audio()
+    tone1 = Sound(duration = rhythm, volume = 100, pitch = "G#4")
 
     tone1.play()
-    tone1.wait_done()
+
 #!/usr/bin/env python3
 
 
