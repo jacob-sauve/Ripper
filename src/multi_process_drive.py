ecc758846a0e184620e614f952094bf105b251cb
IvanGaspart <ivan.torrefacteuramical@gmail.com>
2026-03-24 21:54:17 -0400

    increase distance for demonstration purposes

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -423,7 +423,7 @@ if __name__ == "__main__":
         import titlecard
         titlecard.show()
         print(f"{cpu_count()=}\n\n")
-        brain.queue.put_nowait(("GO", 20, 270))
+        brain.queue.put_nowait(("GO", 50, 270))
         brain.queue.put_nowait(("GRAB", 10, 500))
         brain.queue.put_nowait(("SWEEP", 180, True, 200))
         while not stop.is_pressed():
