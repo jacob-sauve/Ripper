4618bfb4433d87c46c1ab91e4a5332ee67ba682e
IvanGaspart <ivan.torrefacteuramical@gmail.com>
2026-03-24 21:03:47 -0400

    changed musical architecture

--- src/musical.py
+++ src/musical.py
@@ -17,22 +17,18 @@ def victor_jingle():
         ("A3", rhythm*3/8), ("F3", rhythm/4), ("G3", rhythm/2),
         ("C4", rhythm/8), ("D4", rhythm/8), ("Eb4", rhythm*3/8),
         ("C4", rhythm*3/8), ("G#3", rhythm/4), ("Bb3", rhythm/2),
-        ("Eb4", rhythm/8), ("F4", rhythm/8), ("F#4", rhythm*3/8),
+        ("Eb4", rhythm/2), ("F4", rhythm/2), ("F#4", rhythm*3/8),
         ("Eb4", rhythm*3/8), ("B3", rhythm/4), ("Db4", rhythm*3/8),
         ("F#4", rhythm*3/8), ("G#4", rhythm),
     ]
 
-    current = Sound(duration=notes[0][1], pitch=notes[0][0], volume=100)
-    current.play()
+    # pre-compute all sounds
+    sounds = [Sound(duration=dur, pitch=pitch, volume=100) for pitch, dur in notes]
 
-    for pitch, dur in notes[1:]:
-        # prepare next note when current is playing
-        nxt = Sound(duration=dur, pitch=pitch, volume=100)
-        current.wait_done()  # wait for current to finish
-        nxt.play()           
-        current = nxt        
+    for s in sounds:
+        s.play()
+        s.wait_done()
 
-    current.wait_done()
 
 #!/usr/bin/env python3
 # main loop
