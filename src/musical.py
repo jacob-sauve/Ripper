c287ce46098b370d62a26ff04074c82f42e6da3d
IvanGaspart <ivan.torrefacteuramical@gmail.com>
2026-03-24 21:37:31 -0400

    delay stopping

--- src/musical.py
+++ src/musical.py
@@ -9,39 +9,43 @@ import array
 
 from utils.sound import Sound, Song
 
+def make_sounds():
 # duration of a full note (float possible)
-rhythm = 0.75
+    rhythm = 0.75
 
-chord_dur = 4*rhythm
-chord_tones = ["G#3", "C4", "D#4", "G4", "A#4"]
-chord_sounds = [Sound(duration=chord_dur, pitch=p, volume=80) for p in chord_tones]
+    chord_dur = 4*rhythm
+    chord_tones = ["G#3", "C4", "D#4", "G4", "A#4"]
+    chord_sounds = [Sound(duration=chord_dur, pitch=p, volume=80) for p in chord_tones]
 
 # mix them by summing the audio arrays
-mixed = array.array('h', [0] * len(chord_sounds[0].audio))
-for s in chord_sounds:
-    for i in range(len(mixed)):
-        mixed[i] = max(-32768, min(32767, mixed[i] + s.audio[i] // len(chord_tones)))
+    mixed = array.array('h', [0] * len(chord_sounds[0].audio))
+    for s in chord_sounds:
+        for i in range(len(mixed)):
+            mixed[i] = max(-32768, min(32767, mixed[i] + s.audio[i] // 2))
 
-chord = Sound(duration=chord_dur, pitch="A4", volume=100)
-chord.audio = mixed
-
-
-def victor_jingle():
+    chord = Sound(duration=chord_dur, pitch="A4", volume=100)
+    chord.audio = mixed
+    
     notes = [
-        ("A3", rhythm/8), ("B3", rhythm/8), ("C4", rhythm*3/8),
-        ("A3", rhythm*3/8), ("F3", rhythm/4), ("G3", rhythm/2),
-        ("C4", rhythm/8), ("D4", rhythm/8), ("Eb4", rhythm*3/8),
-        ("C4", rhythm*3/8), ("G#3", rhythm/4), ("Bb3", rhythm/2),
-        ("Eb4", rhythm/2), ("F4", rhythm/2), ("F#4", rhythm*3/8),
-        ("Eb4", rhythm*3/8), ("B3", rhythm/4), ("Db4", rhythm*3/8),
-        ("F#4", rhythm*3/8), ("G#4", rhythm),
-    ]
+            ("A3", rhythm/8), ("B3", rhythm/8), ("C4", rhythm*3/8),
+            ("A3", rhythm*3/8), ("F3", rhythm/4), ("G3", rhythm/2),
+            ("C4", rhythm/8), ("D4", rhythm/8), ("Eb4", rhythm*3/8),
+            ("C4", rhythm*3/8), ("G#3", rhythm/4), ("Bb3", rhythm/2),
+            ("Eb4", rhythm/2), ("F4", rhythm/2), ("F#4", rhythm*3/8),
+            ("Eb4", rhythm*3/8), ("B3", rhythm/4), ("Db4", rhythm*3/8),
+            ("F#4", rhythm*3/8), ("G#4", rhythm),
+        ]
 
     sounds = [Sound(duration=dur, pitch=pitch, volume=100) for pitch, dur in notes] 
     sounds.append(chord)
 
     song = Song(sounds)
     song.compile()  # merges everything into one buffer
+    return song
+
+song = make_sounds()
+
+def victor_jingle():
     song.play()
     song.wait_done()
 
