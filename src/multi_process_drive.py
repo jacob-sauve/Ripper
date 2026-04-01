059e04375fcaf65c2ce95ad242ffb4346de34c4e
jacob-sauve <woozy-army-trivial@duck.com>
2026-04-01 14:53:32 -0400

    0.01 polling period gyro

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -544,7 +544,7 @@ class Vision(Processor):
 # main loop
 if __name__ == "__main__":
     processors = {
-            "GYRO": Vision("GYRO", 3, 0.012),
+            "GYRO": Vision("GYRO", 3, 0.01),
             "LEFT": Driver("LEFT", "D"),
             "RIGHT": Driver("RIGHT", "A"),
             "GRABBER": Driver("GRABBER", "B"),
