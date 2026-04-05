676ccdf187ea8f24f3452c769d66475d7fbbb88b
Zachary Chuang <zmchuang365@gmail.com>
2026-04-05 17:06:59 -0400

    color calibration

--- src/multi_process_drive.py
+++ src/multi_process_drive.py
@@ -587,7 +587,7 @@ class Vision(Processor):
         rgb = self.sensor_pin.get_rgb()
         output = dict()
         if rgb != None and not None in rgb:
-            color = classify(rgb, debugging=False)  # SET TO TRUE FOR CALIBRATION
+            color = classify(rgb, debugging=True)  # SET TO TRUE FOR CALIBRATION
             output["color"] = color
             output["rgb"] = rgb
             # print(f"{color = }")
